63ba9eea3c5b6495bfc9514d8103c3d936851c80
GalaxyGorilla <sascha@netdef.org>
2019-11-19 11:31:21 +0000

    Alpha version of config processing (no zebra so far)

--- pathd/path_nb_config.c
+++ pathd/path_nb_config.c
@@ -28,337 +28,433 @@
  * XPath: /frr-pathd:pathd/segment-list
  */
 int pathd_te_segment_list_create(enum nb_event event, const struct lyd_node *dnode,
-			  union nb_resource *resource)
+              union nb_resource *resource)
 {
-	return NB_OK;
+    struct te_segment_list *te_segment_list;
+    const char *name;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    name = yang_dnode_get_string(dnode, "./name");
+    te_segment_list = te_segment_list_create(strdup(name));
+    nb_running_set_entry(dnode, te_segment_list);
+
+    return NB_OK;
 }
 
 int pathd_te_segment_list_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    struct te_segment_list *te_segment_list;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_segment_list = nb_running_unset_entry(dnode);
+    te_segment_list_del(te_segment_list);
+
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/segment-list/label
  */
 int pathd_te_segment_list_label_create(enum nb_event event, const struct lyd_node *dnode,
-			  union nb_resource *resource)
+              union nb_resource *resource)
 {
-	return NB_OK;
+    mpls_label_t label;
+    struct te_segment_list *te_segment_list;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_segment_list = nb_running_get_entry(dnode, NULL, true);
+    label = yang_dnode_get_uint32(dnode, NULL);
+    te_segment_list_label_add(te_segment_list, label);
+
+    return NB_OK;
 }
 
 int pathd_te_segment_list_label_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 int pathd_te_segment_list_label_move(enum nb_event event,
-					   const struct lyd_node *dnode)
+                       const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy
  */
 int pathd_te_sr_policy_create(enum nb_event event, const struct lyd_node *dnode,
-			  union nb_resource *resource)
+              union nb_resource *resource)
 {
-	return NB_OK;
+    struct te_sr_policy *te_sr_policy;
+    const char *name;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    name = yang_dnode_get_string(dnode, "./name");
+    te_sr_policy = te_sr_policy_create(strdup(name));
+    nb_running_set_entry(dnode, te_sr_policy);
+
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    struct te_sr_policy *te_sr_policy;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_sr_policy = nb_running_unset_entry(dnode);
+    te_sr_policy_del(te_sr_policy);
+
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/color
  */
 int pathd_te_sr_policy_color_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	return NB_OK;
+    uint32_t color;
+    struct te_sr_policy *te_sr_policy;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_sr_policy = nb_running_get_entry(dnode, NULL, true);
+    color = yang_dnode_get_uint32(dnode, NULL);
+    te_sr_policy_color_add(te_sr_policy, color);
+
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_color_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/endpoint
  */
 int pathd_te_sr_policy_endpoint_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	return NB_OK;
+    struct ipaddr endpoint;
+    struct te_sr_policy *te_sr_policy;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_sr_policy = nb_running_get_entry(dnode, NULL, true);
+    yang_dnode_get_ip(&endpoint, dnode, NULL);
+    te_sr_policy_endpoint_add(te_sr_policy, &endpoint);
+
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_endpoint_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/binding-sid
  */
 int pathd_te_sr_policy_binding_sid_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	return NB_OK;
+    mpls_label_t binding_sid;
+    struct te_sr_policy *te_sr_policy;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_sr_policy = nb_running_get_entry(dnode, NULL, true);
+    binding_sid = yang_dnode_get_uint32(dnode, NULL);
+    te_sr_policy_binding_sid_add(te_sr_policy, binding_sid);
+
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_binding_sid_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/candidate-path
  */
 int pathd_te_sr_policy_candidate_path_create(enum nb_event event, const struct lyd_node *dnode,
-			  union nb_resource *resource)
+              union nb_resource *resource)
 {
-	return NB_OK;
+    struct te_sr_policy *te_sr_policy;
+    uint32_t preference;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_sr_policy = nb_running_get_entry(dnode, NULL, true);
+    preference = yang_dnode_get_uint32(dnode, "./preference");
+    te_sr_policy_candidate_path_add(te_sr_policy, preference);
+
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_candidate_path_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/candidate-path/protocol-origin
  */
 int pathd_te_sr_policy_candidate_path_protocol_origin_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_candidate_path_protocol_origin_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/candidate-path/originator
  */
 int pathd_te_sr_policy_candidate_path_originator_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_candidate_path_originator_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/candidate-path/dynamic-flag
  */
 int pathd_te_sr_policy_candidate_path_dynamic_flag_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_candidate_path_dynamic_flag_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/sr-policy/candidate-path/segment-list-name
  */
 int pathd_te_sr_policy_candidate_path_segment_list_name_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	return NB_OK;
+    const char *segment_list_name;
+    struct te_sr_policy *te_sr_policy;
+
+    if (event != NB_EV_APPLY)
+        return NB_OK;
+
+    te_sr_policy = nb_running_get_entry(dnode, NULL, true);
+    segment_list_name = yang_dnode_get_string(dnode, NULL);
+    te_sr_policy_candidate_path_segment_list_name_add(te_sr_policy, strdup(segment_list_name));
+
+    return NB_OK;
 }
 
 int pathd_te_sr_policy_candidate_path_segment_list_name_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/mpls/lsp
  */
 int pathd_mpls_lsp_create(enum nb_event event, const struct lyd_node *dnode,
-			  union nb_resource *resource)
+              union nb_resource *resource)
 {
-	struct te_path *path;
-	mpls_label_t input_label;
+    struct te_path *path;
+    mpls_label_t input_label;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	input_label = yang_dnode_get_uint32(dnode, "./input-label");
+    input_label = yang_dnode_get_uint32(dnode, "./input-label");
 
-	path = te_path_add(input_label);
-	nb_running_set_entry(dnode, path);
+    path = te_path_add(input_label);
+    nb_running_set_entry(dnode, path);
 
-	return NB_OK;
+    return NB_OK;
 }
 
 int pathd_mpls_lsp_destroy(enum nb_event event, const struct lyd_node *dnode)
 {
-	struct te_path *path;
+    struct te_path *path;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	path = nb_running_unset_entry(dnode);
-	te_path_del(path);
+    path = nb_running_unset_entry(dnode);
+    te_path_del(path);
 
-	return NB_OK;
+    return NB_OK;
 }
 
 void pathd_mpls_lsp_apply_finish(const struct lyd_node *dnode)
 {
-	struct te_path *path;
+    struct te_path *path;
 
-	path = nb_running_get_entry(dnode, NULL, true);
-	te_path_install_zebra(path);
+    path = nb_running_get_entry(dnode, NULL, true);
+    te_path_install_zebra(path);
 }
 
 /*
  * XPath: /frr-pathd:pathd/mpls/lsp/nhlfe
  */
 int pathd_mpls_lsp_nhlfe_create(enum nb_event event,
-				const struct lyd_node *dnode,
-				union nb_resource *resource)
+                const struct lyd_node *dnode,
+                union nb_resource *resource)
 {
-	struct te_path *path;
-	struct te_path_nhlfe *nhlfe;
-	struct ipaddr nexthop;
+    struct te_path *path;
+    struct te_path_nhlfe *nhlfe;
+    struct ipaddr nexthop;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	path = nb_running_get_entry(dnode, NULL, true);
-	yang_dnode_get_ip(&nexthop, dnode, "./nexthop");
+    path = nb_running_get_entry(dnode, NULL, true);
+    yang_dnode_get_ip(&nexthop, dnode, "./nexthop");
 
-	nhlfe = te_path_nhlfe_add(path, &nexthop);
-	nb_running_set_entry(dnode, nhlfe);
+    nhlfe = te_path_nhlfe_add(path, &nexthop);
+    nb_running_set_entry(dnode, nhlfe);
 
-	return NB_OK;
+    return NB_OK;
 }
 
 int pathd_mpls_lsp_nhlfe_destroy(enum nb_event event,
-				 const struct lyd_node *dnode)
+                 const struct lyd_node *dnode)
 {
-	struct te_path_nhlfe *nhlfe;
+    struct te_path_nhlfe *nhlfe;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	nhlfe = nb_running_unset_entry(dnode);
-	te_path_nhlfe_del(nhlfe);
+    nhlfe = nb_running_unset_entry(dnode);
+    te_path_nhlfe_del(nhlfe);
 
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/mpls/lsp/nhlfe/interface
  */
 int pathd_mpls_lsp_nhlfe_interface_modify(enum nb_event event,
-					  const struct lyd_node *dnode,
-					  union nb_resource *resource)
+                      const struct lyd_node *dnode,
+                      union nb_resource *resource)
 {
-	struct te_path_nhlfe *nhlfe;
-	const char *ifname;
+    struct te_path_nhlfe *nhlfe;
+    const char *ifname;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	nhlfe = nb_running_get_entry(dnode, NULL, true);
-	ifname = yang_dnode_get_string(dnode, NULL);
-	strlcpy(nhlfe->ifname, ifname, sizeof(nhlfe->ifname));
+    nhlfe = nb_running_get_entry(dnode, NULL, true);
+    ifname = yang_dnode_get_string(dnode, NULL);
+    strlcpy(nhlfe->ifname, ifname, sizeof(nhlfe->ifname));
 
-	return NB_OK;
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/mpls/lsp/nhlfe/labels/label
  */
 int pathd_mpls_lsp_nhlfe_labels_label_create(enum nb_event event,
-					     const struct lyd_node *dnode,
-					     union nb_resource *resource)
+                         const struct lyd_node *dnode,
+                         union nb_resource *resource)
 {
-	struct te_path_nhlfe *nhlfe;
-	mpls_label_t label;
+    struct te_path_nhlfe *nhlfe;
+    mpls_label_t label;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	nhlfe = nb_running_get_entry(dnode, NULL, true);
-	label = yang_dnode_get_uint32(dnode, NULL);
-	nhlfe->labels[nhlfe->label_num++] = label;
+    nhlfe = nb_running_get_entry(dnode, NULL, true);
+    label = yang_dnode_get_uint32(dnode, NULL);
+    nhlfe->labels[nhlfe->label_num++] = label;
 
-	return NB_OK;
+    return NB_OK;
 }
 
 int pathd_mpls_lsp_nhlfe_labels_label_destroy(enum nb_event event,
-					      const struct lyd_node *dnode)
+                          const struct lyd_node *dnode)
 {
 #if 0
-	struct te_path_nhlfe *nhlfe;
-	mpls_label_t label;
+    struct te_path_nhlfe *nhlfe;
+    mpls_label_t label;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	nhlfe = nb_running_get_entry(dnode, NULL, true);
-	label = yang_dnode_get_uint32(dnode, NULL);
+    nhlfe = nb_running_get_entry(dnode, NULL, true);
+    label = yang_dnode_get_uint32(dnode, NULL);
 #endif
-	/* TODO: implement me. */
-	return NB_OK;
+    /* TODO: implement me. */
+    return NB_OK;
 }
 
 int pathd_mpls_lsp_nhlfe_labels_label_move(enum nb_event event,
-					   const struct lyd_node *dnode)
+                       const struct lyd_node *dnode)
 {
 #if 0
-	struct te_path_nhlfe *nhlfe;
-	mpls_label_t label;
+    struct te_path_nhlfe *nhlfe;
+    mpls_label_t label;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	nhlfe = nb_running_get_entry(dnode, NULL, true);
-	label = yang_dnode_get_uint32(dnode, NULL);
+    nhlfe = nb_running_get_entry(dnode, NULL, true);
+    label = yang_dnode_get_uint32(dnode, NULL);
 #endif
-	/* TODO: implement me. */
-	return NB_OK;
+    /* TODO: implement me. */
+    return NB_OK;
 }
 
 /*
  * XPath: /frr-pathd:pathd/mpls/lsp/nhlfe/preference
  */
 int pathd_mpls_lsp_nhlfe_preference_modify(enum nb_event event,
-					   const struct lyd_node *dnode,
-					   union nb_resource *resource)
+                       const struct lyd_node *dnode,
+                       union nb_resource *resource)
 {
-	struct te_path_nhlfe *nhlfe;
-	uint32_t pref;
+    struct te_path_nhlfe *nhlfe;
+    uint32_t pref;
 
-	if (event != NB_EV_APPLY)
-		return NB_OK;
+    if (event != NB_EV_APPLY)
+        return NB_OK;
 
-	nhlfe = nb_running_get_entry(dnode, NULL, true);
-	pref = yang_dnode_get_uint32(dnode, NULL);
-	nhlfe->pref = pref;
+    nhlfe = nb_running_get_entry(dnode, NULL, true);
+    pref = yang_dnode_get_uint32(dnode, NULL);
+    nhlfe->pref = pref;
 
-	return NB_OK;
+    return NB_OK;
 }
--- pathd/pathd.h
+++ pathd/pathd.h
@@ -28,37 +28,80 @@ PREDECL_RBTREE_UNIQ(tree_te_path_nhlfe)
 
 /* TE path NHLFE. */
 struct te_path_nhlfe {
-	/* RB-tree entry. */
-	struct tree_te_path_nhlfe_item entry;
+    /* RB-tree entry. */
+    struct tree_te_path_nhlfe_item entry;
 
-	/* Nexthop address. */
-	struct ipaddr nexthop;
+    /* Nexthop address. */
+    struct ipaddr nexthop;
 
-	/* Nexthop interface. */
-	char ifname[INTERFACE_NAMSIZ];
+    /* Nexthop interface. */
+    char ifname[INTERFACE_NAMSIZ];
 
-	/* Nexthop labels. */
-	uint8_t label_num;
-	mpls_label_t labels[MPLS_MAX_LABELS];
+    /* Nexthop labels. */
+    uint8_t label_num;
+    mpls_label_t labels[MPLS_MAX_LABELS];
 
-	/* Administrative preference. */
-	uint32_t pref;
+    /* Administrative preference. */
+    uint32_t pref;
 
-	/* Back pointer to TE path. */
-	struct te_path *path;
+    /* Back pointer to TE path. */
+    struct te_path *path;
 };
 
 /* TE path. */
 struct te_path {
-	/* RB-tree entry. */
-	struct tree_te_path_item entry;
+    /* RB-tree entry. */
+    struct tree_te_path_item entry;
 
-	/* MPLS LSP input label. */
-	mpls_label_t input_label;
+    /* MPLS LSP input label. */
+    mpls_label_t input_label;
 
-	/* List of NHLFEs. */
-	struct tree_te_path_nhlfe_head nhlfes;
+    /* List of NHLFEs. */
+    struct tree_te_path_nhlfe_head nhlfes;
 };
+
+struct te_segment_list {
+    RB_ENTRY(te_segment_list) entry;
+
+    /* Name of the Segment List. */
+    char *name;
+
+    /* Nexthop labels. */
+    uint8_t label_num;
+    mpls_label_t *labels;
+};
+RB_HEAD(te_segment_list_instance_head, te_segment_list);
+RB_PROTOTYPE(te_segment_list_instance_head, te_segment_list, entry, te_segment_list_instance_compare)
+
+struct te_candidate_path {
+    /* Administrative preference. */
+    uint32_t preference;
+
+    /* The associated Segment List. */
+    char *segment_list_name;
+};
+
+struct te_sr_policy {
+    RB_ENTRY(te_sr_policy) entry;
+
+    /* Name */
+    char *name;
+
+    /* Binding SID */
+    mpls_label_t binding_sid;
+
+    /* Color */
+    uint32_t color;
+
+    /* Endpoint */
+    struct ipaddr *endpoint;
+
+    /* Candidate Paths */
+    uint8_t candidate_path_num;
+    struct te_candidate_path *candidate_paths;
+};
+RB_HEAD(te_sr_policy_instance_head, te_sr_policy);
+RB_PROTOTYPE(te_sr_policy_instance_head, te_sr_policy, entry, te_sr_policy_instance_compare)
 
 extern struct zebra_privs_t pathd_privs;
 extern struct tree_te_path_head te_paths;
@@ -68,21 +111,31 @@ struct te_path *te_path_add(mpls_label_t input_label);
 void te_path_del(struct te_path *path);
 struct te_path *te_path_find(mpls_label_t input_label);
 struct te_path_nhlfe *te_path_nhlfe_add(struct te_path *path,
-					struct ipaddr *nexthop);
+                    struct ipaddr *nexthop);
 void te_path_nhlfe_del(struct te_path_nhlfe *nhlfe);
 struct te_path_nhlfe *te_path_nhlfe_find(struct te_path *path,
-					 struct ipaddr *nexthop);
+                     struct ipaddr *nexthop);
 void te_path_install_zebra(struct te_path *path);
 void te_path_uninstall_zebra(struct te_path *path);
 void path_zebra_init(struct thread_master *master);
 void path_cli_init(void);
 
+struct te_segment_list *te_segment_list_create(char *name);
+void te_segment_list_label_add(struct te_segment_list *te_segment_list, mpls_label_t label);
+void te_segment_list_del(struct te_segment_list *te_segment_list);
+struct te_sr_policy *te_sr_policy_create(char *name);
+void te_sr_policy_del(struct te_sr_policy *te_sr_policy);
+void te_sr_policy_color_add(struct te_sr_policy *te_sr_policy, uint32_t color);
+void te_sr_policy_endpoint_add(struct te_sr_policy *te_sr_policy, struct ipaddr *endpoint);
+void te_sr_policy_binding_sid_add(struct te_sr_policy *te_sr_policy, mpls_label_t binding_sid);
+void te_sr_policy_candidate_path_add(struct te_sr_policy *te_sr_policy, uint32_t preference);
+void te_sr_policy_candidate_path_segment_list_name_add(struct te_sr_policy *te_sr_policy, char *segment_list_name);
+
 /* Generate rb-trees. */
 int te_path_nhlfe_compare(const struct te_path_nhlfe *a,
-			  const struct te_path_nhlfe *b);
+              const struct te_path_nhlfe *b);
 DECLARE_RBTREE_UNIQ(tree_te_path_nhlfe, struct te_path_nhlfe, entry,
-		    te_path_nhlfe_compare)
+            te_path_nhlfe_compare)
 int te_path_compare(const struct te_path *a, const struct te_path *b);
 DECLARE_RBTREE_UNIQ(tree_te_path, struct te_path, entry, te_path_compare)
-
 #endif /* _FRR_PATHD_H_ */
